ea415798a89c7ed1c815bc4b149c4360dfe09e33
Nadja Brix Koch <nadjabrixkoch@hotmail.com>
2023-03-04 12:59:58 +0100

    descriptions for experiences

--- components/cv/experiences/experience-data.ts
+++ components/cv/experiences/experience-data.ts
@@ -19,7 +19,7 @@ const experiences: Company[] = [
         title: "Head tutor",
         years: "2023-Present",
         description:
-          "TA in Algorithms and Data Structures, First Year Project, Distributed Systems, Foundations of Computing - Discrete Mathematics and StudyLab.",
+          "Project management of the study start for bachelor students at ITU, including recruitment of tutors, planning and facilitation of the study start.",
       },
       {
         title: "Teaching Assistant",
@@ -36,7 +36,8 @@ const experiences: Company[] = [
       {
         title: "Student Software Developer",
         years: "2022-2023",
-        description: "",
+        description:
+          "Fullstack development of a web application, as the sole developer in the startup Cyberjuice.",
       },
     ],
   },
@@ -47,7 +48,7 @@ const experiences: Company[] = [
       {
         title: "Barista",
         years: "2020-2021",
-        description: "Volunteer barista at the student café at ITU.clio",
+        description: "Volunteer barista at the student café at ITU.",
       },
     ],
   },
@@ -56,10 +57,33 @@ const experiences: Company[] = [
     link: "https://dk.clio.me/",
     jobs: [
       {
-        title: "Different positions",
-        years: "2015-2020",
+        title: "Portfolio Management Coordinator",
+        years: "2020",
+        description: "Student assistant in the Product Owner team.",
+      },
+      {
+        title: "Platform specialist",
+        years: "2018-2020",
         description:
-          "Including web editor, team lead, platform specialist and project manager.",
+          "Project management of differing projects in Clio, and link between the IT- and editorial department.",
+      },
+      {
+        title: "Team lead for team Upload",
+        years: "2017-2018",
+        description:
+          "Personal management of a small team of 3-4 people, including project management for the team.",
+      },
+      {
+        title: "Uploader",
+        years: "2016",
+        description:
+          "Upload of content to the Clio platform, including metadata, images, validation of layout etc. Additionally, creation of digital media for the platform, such as graphs, pdf-files, simple images.",
+      },
+      {
+        title: "Data validation specialist",
+        years: "2015",
+        description:
+          "Manual validation of data that had been imported into a new subscription management system.",
       },
     ],
   },
