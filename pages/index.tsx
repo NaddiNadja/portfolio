b1ff50c98865e992b17eb05cb8cb62ee00acf005
Nadja Brix Koch <nadjabrixkoch@hotmail.com>
2023-03-04 13:01:40 +0100

    fix of page-title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import CV from "components/cv";
+import React from "react";
+import { PageTitleContext } from "./_app";
 
 const Home = () => {
+  const page = React.useContext(PageTitleContext);
+  React.useEffect(() => {
+    page.setTitle("CV");
+  }, [page]);
+
   return <CV />;
 };
 
